f9e31ba633f4b6cc8606efba0eab50705158b42e
namphan06 <namphan06124@gmail.com>
2024-06-26 15:01:31 +0700

    np

---
+++ Tuấn Anh/Header $ Footer/header.js
@@ -0,0 +1,13 @@
+
+document.querySelector(".menu a").addEventListener("click", function (event) {
+    event.preventDefault(); // Ngăn chặn hành động mặc định của liên kết
+    document.querySelector(".overlay").style.display = "block";
+    document.querySelector(".sidebar").style.left = "0";
+  });
+  
+  document.querySelector(".overlay").addEventListener("click", function () {
+    this.style.display = "none";
+    document.querySelector(".sidebar").style.left = "-20%";
+  });
+  
+  
